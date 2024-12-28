b66e9fc504d9cd6e5d655e19e56afeb9700be466
LokeshLenka <lenkalokesh98@gmail.com>
2024-12-28 11:19:34 +0530

    updated nginx and vite to deliver content over https

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: "resources/js/app.tsx",
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        https: true,
+        host: "0.0.0.0",
+        hmr: {
+            host: "phone-32h0.onrender.com",
+            protocol: "https",
+        },
+    },
 });
